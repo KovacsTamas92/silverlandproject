50d3a2da11133bc3d18915fced40b984e33a8337
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2024-07-31 12:26:36 +0200

    fixed code

--- src/admin/pages/AdminUpload.jsx
+++ src/admin/pages/AdminUpload.jsx
@@ -163,10 +163,10 @@ const AdminUpload = () => {
     };
 
     return (
-        <div>
-            <AdminNavbar />
-            <div className="mt-20 p-4">
-                <form onSubmit={handleSubmit}>
+        <div className="flex justify-center">
+            <div className="mt-20 p-4 max-w-2xl w-full">
+                <AdminNavbar />
+                <form onSubmit={handleSubmit} className="space-y-4">
                     <div className="mb-4">
                         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="maincategory">
                             Fő Kategória
